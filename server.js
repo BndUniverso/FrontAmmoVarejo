e00be73fa17cd7466d8895e5551b885d0bd11ec1
LJLeandro <lucasjleandro@gmail.com>
2018-10-17 00:43:39 -0300

    primeiro deploy

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var path = require('path');
 var express = require('express');
 const app = express();
  
-app.use(express.static(__dirname + '/dist'));
-
 app.listen(process.env.PORT || 8080);
 
 app.get('*', (req, res) => {
