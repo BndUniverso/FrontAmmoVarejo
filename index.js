7244a54b16450113aeef7458f23ee37598455871
LJLeandro <lucasjleandro@gmail.com>
2018-10-16 23:49:25 -0300

    primeiro deploy

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT);
 
 app.get('/*', function (req, res) {
+  console.log("Aqui abestado...");
+
   res.sendfile(path.join(__dirname + '/dist/index.html'));
 });
 
