84065a670770717675e7250456af175dd677ff49
LJLeandro <lucasjleandro@gmail.com>
2018-10-17 00:16:48 -0300

    primeiro deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
  
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
 
 app.get('*', (req, res) => {
   res.sendFile((__dirname + '/dist/front-end/index.html')); // load the single view file (angular will handle the page changes on the front-end)
