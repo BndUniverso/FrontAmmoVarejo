552865b58b9890bfc8270fd4dbafea16f5b8e937
LJLeandro <lucasjleandro@gmail.com>
2018-10-16 23:44:53 -0300

    primeiro deploy

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ var path = require('path');
 var express = require('express');
 var app = express();
  
-app.set('port', (process.env.PORT || 5000));
- 
-app.use(express.static(path.join(__dirname, 'dist')));
- 
-// redireciona todas as requições para o Angular 2 
-app.all('*', function(req, res) {
-  res.status(200).sendFile(
-  	path.join(__dirname, 'dist', 'index.html'));
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT);
+
+app.get('/*', function (req, res) {
+  res.sendfile(path.join(__dirname + '/dist/index.html'));
 });
- 
-app.listen(app.get('port'), function() {
-  console.log('Node executando na porta ', app.get('port'));
-});
+
