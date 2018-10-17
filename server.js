79a7aa08b9610c397f0b98a7e09cf9a08d44a2c9
LJLeandro <lucasjleandro@gmail.com>
2018-10-17 00:50:43 -0300

    Agora vai

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-var path = require('path');
-var express = require('express');
+const express = require('express');
 const app = express();
- 
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist'));
+
 app.listen(process.env.PORT || 8080);
 
-app.get('*', (req, res) => {
-  res.sendFile((__dirname + '/dist/front-end/index.html')); // load the single view file (angular will handle the page changes on the front-end)
-});
+app.get('/*', function(req, res) {
+res.sendFile(path.join(__dirname + '/dist/front-end/index.html'));
+})
+
+console.log('Console listening!');
